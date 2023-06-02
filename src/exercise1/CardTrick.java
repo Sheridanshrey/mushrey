16bcf577bdc8e58dec87d26291bddf3db4de3b41
Sheridanshrey <134073837+Sheridanshrey@users.noreply.github.com>
2023-06-02 10:32:52 -0400

    Update CardTrick.java

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -13,7 +13,7 @@ public class CardTrick {
     public static void main(String[] args) {
         
         Card[] hand = new Card[7];
-
+//PRINTINFO();
         for (int i = 0; i < hand.length; i++) {
             Card card = new Card();
             //card.setValue(insert call to random number generator here)
